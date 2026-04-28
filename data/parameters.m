73045e79918abc4484cb87334d48423e44a3023c
Abd-Alaj <144612314+abd-alaj@users.noreply.github.com>
2026-04-28 05:30:05 -0400

    added FOC control architecture in library

--- data/parameters.m
+++ data/parameters.m
@@ -11,7 +11,7 @@ kn = 117;
 J_intert = 56e-7;
 
 % switch this out with actual experimentally determined K_e once BLDC motor is properly sourced
-K_e = (1 / kn) (2 * pi / 60); 
+K_e = (1 / kn) * (2 * pi / 60); 
 
 % operational safety limits
 V_nom = 12;
@@ -21,8 +21,12 @@ T_stall = 0.15;
 max_rpm = 1610; 
 
 % chosen parameters
-f_sw = 30e-3;
+Vdc = 12;
+f_sw = 30e3;
 T_s = 1 / f_sw;
 V_dc = 12; 
 
 lambda = Kt / (1.5 * P);
+
+% controller parameters
+target_speed = 400;
